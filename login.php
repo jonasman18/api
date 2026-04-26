cb0bae138b808893b592f74f001e0d46b98531ab
jonasman18 <buancoheriniaina@gmail.com>
2026-04-26 18:39:43 +0300

    Fix: remove duplicate CORS headers

--- login.php
+++ login.php
@@ -4,9 +4,7 @@ require_once __DIR__ . '/vendor/autoload.php';
 use Firebase\JWT\JWT;
 
 header('Content-Type: application/json');
-header("Access-Control-Allow-Origin: https://visiteur.vercel.app");
-header("Access-Control-Allow-Headers: Content-Type, Authorization");
-header("Access-Control-Allow-Methods: POST, OPTIONS");
+
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
