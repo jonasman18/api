7f72bfc5f220cea5bcd83301e7984dc9fa29943a
jonasman18 <buancoheriniaina@gmail.com>
2026-04-27 13:14:53 +0300

    Fix: remove duplicate CORS headers in register.php

--- register.php
+++ register.php
@@ -1,8 +1,6 @@
 <?php
 header('Content-Type: application/json');
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: POST");
-header("Access-Control-Allow-Headers: Content-Type");
+
 
 // 🔥 CONNEXION RAILWAY
 $host = 'maglev.proxy.rlwy.net';
